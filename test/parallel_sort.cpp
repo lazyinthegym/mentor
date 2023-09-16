e7c719de92a90301821e2b4602fea8c3450d9bdc
lazyinthegym <ahmad.m.ismael92@gmail.com>
2023-09-16 09:59:47 +0200

    implement merge_sub_ranges

--- test/parallel_sort.cpp
+++ test/parallel_sort.cpp
@@ -4,6 +4,7 @@
 #include <thread>
 #include <algorithm>
 #include <fstream>
+#include <queue>
 #include "timer.h"
 
 namespace {
@@ -34,92 +35,164 @@ std::vector<std::string> read_shuffled_words(){
   return lines;
 }
 
-// Function to merge two sorted sub-ranges into a single sorted sub-range
-void mergeSortedRanges(std::vector<std::string>& arr, int left, int middle, int right) {
-  std::vector<std::string> temp(right - left + 1);
-  int i = left, j = middle + 1, k = 0;
+void merge_sub_ranges(std::vector<std::string>& arr, std::pair<int, int> first, std::pair<int, int> second) {
+  // Assert assumptions
+  assert(first.second < second.first);
+  assert(first.second + 1 == second.first);
 
-  while (i <= middle && j <= right) {
+  std::vector<std::string> merged;
+  int i = first.first;
+  int j = second.first;
+
+  while (i <= first.second && j <= second.second) {
     if (arr[i] <= arr[j]) {
-      temp[k++] = arr[i++];
+      merged.push_back(arr[i]);
+      i++;
     } else {
-      temp[k++] = arr[j++];
+      merged.push_back(arr[j]);
+      j++;
     }
   }
 
-  while (i <= middle) {
-    temp[k++] = arr[i++];
+  // Copy any remaining elements from the first subrange
+  while (i <= first.second) {
+    merged.push_back(arr[i]);
+    i++;
   }
 
-  while (j <= right) {
-    temp[k++] = arr[j++];
+  // Copy any remaining elements from the second subrange
+  while (j <= second.second) {
+    merged.push_back(arr[j]);
+    j++;
   }
 
-  // Copy the merged values back into the original array
-  for (i = 0; i < k; ++i) {
-    arr[left + i] = temp[i];
-  }
-}
-
-void parallel_sort(std::vector<std::string>& data, int num_threads) {
-
-  // Calculate the number of elements each thread will handle
-  size_t chunk_size = data.size() / num_threads;
-
-//  assert(chunk_size * num_threads == data.size());
-
-  // Create and launch threads to sort sub-ranges
-  std::vector<std::thread> threads;
-  for (int i = 0; i < num_threads; ++i) {
-    int left = i * chunk_size;
-    int right = (i == num_threads - 1) ? data.size() : (i + 1) * chunk_size;
-
-    threads.emplace_back([&data, left, right](){
-      std::sort(data.begin() + left, data.begin() + right);
-    });
-  }
-
-  // Wait for all threads to finish
-  for (std::thread& t : threads) {
-    t.join();
-  }
-
-  // Merge the sorted sub-ranges (your existing code)
-  for (int i = 1; i <= num_threads; ++i) {
-    int middle = i * chunk_size - 1;
-    int right = (i + 1) * chunk_size - 1;
-    if (right >= data.size()) {
-      right = data.size() - 1;
-    }
-    mergeSortedRanges(data, 0, middle, right);
+  // Copy the merged result back to the original array
+  for (int k = 0; k < merged.size(); k++) {
+    arr[first.first + k] = merged[k];
   }
 }
+
+//void parallel_sort(std::vector<std::string>& data, int num_threads) {
+//
+//  // Calculate the number of elements each thread will handle
+//  size_t chunk_size = data.size() / num_threads;
+//
+//  std::set<std::pair<int,int>> init_sub_ranges{};
+//
+//  // Create and launch threads to sort sub-ranges
+//  std::vector<std::thread> threads;
+//  for (int i = 0; i < num_threads; ++i) {
+//    int left = i * chunk_size;
+//    int right = (i == num_threads - 1) ? data.size() : (i + 1) * chunk_size;
+//
+//    threads.emplace_back([&data, left, right](){
+//      std::sort(data.begin() + left, data.begin() + right);
+//    });
+//
+//    init_sub_ranges.emplace(left, right - 1);
+//  }
+//
+//  // Wait for all threads to finish
+//  for (std::thread& t : threads) {
+//    t.join();
+//  }
+//
+//  // Merge the sorted sub-ranges
+//
+//  while(init_sub_ranges.size() != 1) { // This means that all sub-ranges are merged
+//    // Create and launch threads to merge sub-ranges
+//    std::vector<std::thread> merging_threads;
+//
+//    std::set<std::pair<int,int>> result_sub_ranges{};
+//
+//    while (init_sub_ranges.size() > 1) {
+//      // Merge the first 2 ranges
+//      auto first_range = *init_sub_ranges.begin();
+//      auto second_range = *std::next(init_sub_ranges.begin(), 1);
+//      merging_threads.emplace_back([&]{mergeSortedRanges(data, first_range.first, first_range.second , second_range.second);});
+//
+//      // Erase the 2 ranges and put their combined range in result_sub_ranges
+//      init_sub_ranges.erase(init_sub_ranges.begin());
+//      init_sub_ranges.erase(init_sub_ranges.begin());
+//      result_sub_ranges.emplace(first_range.first, second_range.second);
+//    }
+//
+//    init_sub_ranges.insert(result_sub_ranges.begin(), result_sub_ranges.end());
+//
+//    // Join all merging threads
+//    for (std::thread& t : merging_threads) {
+//      t.join();
+//    }
+//
+//  }
+//
+////  for (int i = 1; i <= num_threads; ++i) {
+////    int middle = i * chunk_size - 1;
+////    int right = (i + 1) * chunk_size - 1;
+////    if (right >= data.size()) {
+////      right = data.size() - 1;
+////    }
+////    mergeSortedRanges(data, 0, middle, right);
+////  }
+//}
 
 } // namespace
 
-TEST(ParallelSort, CalculateSpeedUp) {
-  std::vector<std::string> data = read_shuffled_words();
-
+//TEST(ParallelSort, CalculateSpeedUp) {
+//  std::vector<std::string> data = read_shuffled_words();
+//
 //  data.resize(100);
+//
+//  // Create two different copies for each case
+//  std::vector<std::string> data_1(data);
+//  std::vector<std::string> data_2(data);
+//
+//  // Single Thread Sorting
+//  Timer single_timer;
+//  std::sort(data_1.begin(), data_1.end());
+//  auto single_thread_time = single_timer.elapsed_milliseconds();
+//
+//  // Parallel Merge Sorting
+//  int num_threads = std::thread::hardware_concurrency(); // Use the number of available CPU cores
+//  Timer multi_timer;
+//  parallel_sort(data_2, num_threads - 1);
+//  auto multi_thread_time = multi_timer.elapsed_milliseconds();
+//
+//  std::cout << "Using a Parallel Merge Sort with "<< num_threads << " threads, the speed up = "  << single_thread_time/multi_thread_time << std::endl;
+//
+//  //Compare with the std::sort result to test its correctness
+//
+//  EXPECT_EQ(data_1, data_2);
+//}
 
-  // Create two different copies for each case
-  std::vector<std::string> data_1(data);
-  std::vector<std::string> data_2(data);
 
-  // Single Thread Sorting
-  Timer single_timer;
-  std::sort(data_1.begin(), data_1.end());
-  auto single_thread_time = single_timer.elapsed_milliseconds();
+TEST(MergeSubRangesTest, ComprehensiveTest) {
+  // Test Case 1: Merge Sorted Sub-Ranges
+  {
+    std::vector<std::string> arr = {"apple", "banana", "cherry", "date", "fig", "grape", "kiwi", "lemon"};
+    std::pair<int, int> first = {0, 3};
+    std::pair<int, int> second = {4, 7};
 
-  // Parallel Merge Sorting
-  int num_threads = std::thread::hardware_concurrency(); // Use the number of available CPU cores
-  Timer multi_timer;
-  parallel_sort(data_2, num_threads - 1);
-  auto multi_thread_time = multi_timer.elapsed_milliseconds();
+    merge_sub_ranges(arr, first, second);
 
-  std::cout << "Using a Parallel Merge Sort with "<< num_threads << " threads, the speed up = "  << single_thread_time/multi_thread_time << std::endl;
+    // Verify that the merged sub-ranges are sorted correctly
+    EXPECT_TRUE(std::is_sorted(arr.begin(), arr.end()));
 
-  //Compare with the std::sort result to test its correctness
+    // Verify the merged result
+    std::vector<std::string> expected = {"apple", "banana", "cherry", "date", "fig", "grape", "kiwi", "lemon"};
+    EXPECT_EQ(arr, expected);
+  }
 
-  EXPECT_EQ(data_1, data_2);
+  // Test Case 2: Merge Single Element Ranges
+  {
+    std::vector<std::string> arr = {"apple", "banana", "cherry", "date"};
+    std::pair<int, int> first = {0, 0};
+    std::pair<int, int> second = {1, 1};
+
+    merge_sub_ranges(arr, first, second);
+
+    // Verify the merged result
+    std::vector<std::string> expected = {"apple", "banana", "cherry", "date"};
+    EXPECT_EQ(arr, expected);
+  }
 }
