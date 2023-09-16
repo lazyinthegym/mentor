19ef52c656be77e6802e061d681e4375fb2fab19
lazyinthegym <ahmad.m.ismael92@gmail.com>
2023-09-16 12:37:07 +0200

    fix parallel_sort + add tests

--- test/parallel_sort.cpp
+++ test/parallel_sort.cpp
@@ -8,7 +8,7 @@
 #include "timer.h"
 
 namespace {
-std::vector<std::string> read_shuffled_words(){
+std::vector<std::string> read_shuffled_words() {
   // Input file path
   auto filePath = std::string(RESOURCES_PATH) + "shuffledWords.txt"; // Replace with your file path
 
@@ -17,7 +17,7 @@ std::vector<std::string> read_shuffled_words(){
 
   if (!inputFile.is_open()) {
     std::cerr << "Error: Failed to open input file." << std::endl;
-    exit(1) ;
+    exit(1);
   }
 
 // Create a vector to store lines as strings
@@ -35,7 +35,7 @@ std::vector<std::string> read_shuffled_words(){
   return lines;
 }
 
-void merge_sub_ranges(std::vector<std::string>& arr, std::pair<int, int> first, std::pair<int, int> second) {
+void merge_sub_ranges(std::vector<std::string> &arr, std::pair<int, int> first, std::pair<int, int> second) {
   // Assert assumptions
   assert(first.second < second.first);
   assert(first.second + 1 == second.first);
@@ -72,104 +72,88 @@ void merge_sub_ranges(std::vector<std::string>& arr, std::pair<int, int> first, 
   }
 }
 
-//void parallel_sort(std::vector<std::string>& data, int num_threads) {
-//
-//  // Calculate the number of elements each thread will handle
-//  size_t chunk_size = data.size() / num_threads;
-//
-//  std::set<std::pair<int,int>> init_sub_ranges{};
-//
-//  // Create and launch threads to sort sub-ranges
-//  std::vector<std::thread> threads;
-//  for (int i = 0; i < num_threads; ++i) {
-//    int left = i * chunk_size;
-//    int right = (i == num_threads - 1) ? data.size() : (i + 1) * chunk_size;
-//
-//    threads.emplace_back([&data, left, right](){
-//      std::sort(data.begin() + left, data.begin() + right);
-//    });
-//
-//    init_sub_ranges.emplace(left, right - 1);
-//  }
-//
-//  // Wait for all threads to finish
-//  for (std::thread& t : threads) {
-//    t.join();
-//  }
-//
-//  // Merge the sorted sub-ranges
-//
-//  while(init_sub_ranges.size() != 1) { // This means that all sub-ranges are merged
-//    // Create and launch threads to merge sub-ranges
-//    std::vector<std::thread> merging_threads;
-//
-//    std::set<std::pair<int,int>> result_sub_ranges{};
-//
-//    while (init_sub_ranges.size() > 1) {
-//      // Merge the first 2 ranges
-//      auto first_range = *init_sub_ranges.begin();
-//      auto second_range = *std::next(init_sub_ranges.begin(), 1);
-//      merging_threads.emplace_back([&]{mergeSortedRanges(data, first_range.first, first_range.second , second_range.second);});
-//
-//      // Erase the 2 ranges and put their combined range in result_sub_ranges
-//      init_sub_ranges.erase(init_sub_ranges.begin());
-//      init_sub_ranges.erase(init_sub_ranges.begin());
-//      result_sub_ranges.emplace(first_range.first, second_range.second);
-//    }
-//
-//    init_sub_ranges.insert(result_sub_ranges.begin(), result_sub_ranges.end());
-//
-//    // Join all merging threads
-//    for (std::thread& t : merging_threads) {
-//      t.join();
-//    }
-//
-//  }
-//
-////  for (int i = 1; i <= num_threads; ++i) {
-////    int middle = i * chunk_size - 1;
-////    int right = (i + 1) * chunk_size - 1;
-////    if (right >= data.size()) {
-////      right = data.size() - 1;
-////    }
-////    mergeSortedRanges(data, 0, middle, right);
-////  }
-//}
+void parallel_sort(std::vector<std::string> &data, int num_threads) {
+
+  // Calculate the number of elements each thread will handle
+  size_t chunk_size = data.size() / num_threads;
+
+  std::set<std::pair<int, int>> init_sub_ranges{};
+
+  // Create and launch threads to sort sub-ranges
+  std::vector<std::thread> threads;
+  for (int i = 0; i < num_threads; ++i) {
+    int left = i * chunk_size;
+    int right = (i == num_threads - 1) ? data.size() : (i + 1) * chunk_size;
+
+    threads.emplace_back([&data, left, right]() {
+      std::sort(data.begin() + left, data.begin() + right);
+    });
+
+    init_sub_ranges.emplace(left, right - 1);
+  }
+
+  // Wait for all threads to finish
+  for (std::thread &t : threads) {
+    t.join();
+  }
+
+  // Merge the sorted sub-ranges
+
+  while (init_sub_ranges.size() != 1) { // This means that all sub-ranges are merged
+    // Create and launch threads to merge sub-ranges
+    std::vector<std::thread> merging_threads;
+
+    std::set<std::pair<int, int>> result_sub_ranges{};
+
+    while (init_sub_ranges.size() > 1) {
+      // Merge the first 2 ranges
+      auto first_range = *init_sub_ranges.begin();
+      auto second_range = *std::next(init_sub_ranges.begin(), 1);
+      merging_threads.emplace_back([&data, first_range, second_range] {
+        merge_sub_ranges(data, first_range, second_range);
+      });
+
+      // Erase the 2 ranges and put their combined range in result_sub_ranges
+      init_sub_ranges.erase(init_sub_ranges.begin());
+      init_sub_ranges.erase(init_sub_ranges.begin());
+      result_sub_ranges.emplace(first_range.first, second_range.second);
+    }
+
+    init_sub_ranges.insert(result_sub_ranges.begin(), result_sub_ranges.end());
+
+    // Join all merging threads
+    for (std::thread &t : merging_threads) {
+      t.join();
+    }
+
+  }
+}
 
 } // namespace
 
-//TEST(ParallelSort, CalculateSpeedUp) {
-//  std::vector<std::string> data = read_shuffled_words();
-//
-//  data.resize(100);
-//
-//  // Create two different copies for each case
-//  std::vector<std::string> data_1(data);
-//  std::vector<std::string> data_2(data);
-//
-//  // Single Thread Sorting
-//  Timer single_timer;
-//  std::sort(data_1.begin(), data_1.end());
-//  auto single_thread_time = single_timer.elapsed_milliseconds();
-//
-//  // Parallel Merge Sorting
-//  int num_threads = std::thread::hardware_concurrency(); // Use the number of available CPU cores
-//  Timer multi_timer;
-//  parallel_sort(data_2, num_threads - 1);
-//  auto multi_thread_time = multi_timer.elapsed_milliseconds();
-//
-//  std::cout << "Using a Parallel Merge Sort with "<< num_threads << " threads, the speed up = "  << single_thread_time/multi_thread_time << std::endl;
-//
-//  //Compare with the std::sort result to test its correctness
-//
-//  EXPECT_EQ(data_1, data_2);
-//}
+/***********************************
+***      Correctness Tests      ***
+***********************************/
+TEST(ParallelSort, CorrectnessTest1) {
+  std::vector<std::string> arr = {"kiwi", "apple", "date", "cherry", "grape", "lemon", "banana", "fig", "juice"};
+  parallel_sort(arr, 4);
+  EXPECT_TRUE(std::is_sorted(arr.begin(), arr.end()));
 
+  // Verify the merged result
+  std::vector<std::string> expected = {"apple", "banana", "cherry", "date", "fig", "grape", "juice", "kiwi", "lemon"};
+  EXPECT_EQ(arr, expected);
+}
 
-TEST(MergeSubRangesTest, ComprehensiveTest) {
+TEST(ParallelSort, CorrectnessTest2) {
+  std::vector<std::string> arr = read_shuffled_words();
+  parallel_sort(arr, 9);
+  EXPECT_TRUE(std::is_sorted(arr.begin(), arr.end()));
+}
+
+TEST(MergeSubRangesTest, CorrectnessTest) {
   // Test Case 1: Merge Sorted Sub-Ranges
   {
-    std::vector<std::string> arr = {"apple", "banana", "cherry", "date", "fig", "grape", "kiwi", "lemon"};
+    std::vector<std::string> arr = {"fig", "grape", "kiwi", "lemon", "apple", "banana", "cherry", "date"};
     std::pair<int, int> first = {0, 3};
     std::pair<int, int> second = {4, 7};
 
@@ -183,9 +167,25 @@ TEST(MergeSubRangesTest, ComprehensiveTest) {
     EXPECT_EQ(arr, expected);
   }
 
+  // Test Case 2: Merge Sorted Sub-Ranges
+  {
+    std::vector<std::string> arr = {"apple", "kiwi", "cherry", "date"};
+    std::pair<int, int> first = {0, 1};
+    std::pair<int, int> second = {2, 3};
+
+    merge_sub_ranges(arr, first, second);
+
+    // Verify that the merged sub-ranges are sorted correctly
+    EXPECT_TRUE(std::is_sorted(arr.begin(), arr.end()));
+
+    // Verify the merged result
+    std::vector<std::string> expected = {"apple", "cherry", "date", "kiwi"};
+    EXPECT_EQ(arr, expected);
+  }
+
   // Test Case 2: Merge Single Element Ranges
   {
-    std::vector<std::string> arr = {"apple", "banana", "cherry", "date"};
+    std::vector<std::string> arr = {"banana", "apple", "cherry", "date"};
     std::pair<int, int> first = {0, 0};
     std::pair<int, int> second = {1, 1};
 
@@ -195,4 +195,25 @@ TEST(MergeSubRangesTest, ComprehensiveTest) {
     std::vector<std::string> expected = {"apple", "banana", "cherry", "date"};
     EXPECT_EQ(arr, expected);
   }
+}
+
+/***********************************
+***      Performance Tests      ***
+***********************************/
+
+TEST(ParallelSort, PerformanceTest) {
+  std::vector<std::string> arr_1 = read_shuffled_words();
+  std::vector<std::string> arr_2 = arr_1;
+
+  Timer timer_1;
+  std::sort(arr_1.begin(), arr_1.end());
+  auto std_time = timer_1.elapsed_milliseconds();
+
+  Timer timer_2;
+  parallel_sort(arr_2, std::thread::hardware_concurrency() - 1);
+  auto algo_time = timer_2.elapsed_milliseconds();
+  EXPECT_TRUE(std::is_sorted(arr_2.begin(), arr_2.end()));
+
+  std::cout << "The Speed Up = " << std_time / algo_time << std::endl;
+
 }
